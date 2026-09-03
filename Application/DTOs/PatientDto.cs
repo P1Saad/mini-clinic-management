252f18eb997c84d82330897883f2ff8773ec85ea
Saad <saadxgithub@gmail.com>
2026-09-03 23:34:09 +0300

    Update namespaces

--- Application/DTOs/PatientDto.cs
+++ Application/DTOs/PatientDto.cs
@@ -4,14 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace Application.DTOs
 {
     public class PatientDto : BaseDTOs
     {
+        [Required]
+        [RegularExpression(@"^(?!string$)", ErrorMessage = "Patient name is required.")]
         public string PatientName { get; set; }
+
+
         public string Phone { get; set; }
         public int Age { get; set; }
+        [Required]
+        [RegularExpression(@"^(?!string$)", ErrorMessage = "Patient gender is required.")]
+
         public string Gender { get; set; }
     }
 }
